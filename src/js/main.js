d0f3363d5b4e41929d0591298d0172a5052162b2
Arne Sierens <arne.srns@gmail.com>
2015-09-28 20:43:55 +0200

    Added messages dropdown list and click events to show messages and profile settings

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,29 @@ $(document).ready(function() {
     $('#global-search-field').focusout(function() {
         $('#dropdown-search').hide();
     });
+    
+    // Profile dropdown
+    $('#header-profile').click(function(e) {
+        e.preventDefault();
+        $('#header-profile-dropdown').toggle();
+    });
+    
+    // Messages dropdown
+    $('#header-messages').click(function(e) {
+        e.preventDefault();
+        $('#header-messages-dropdown').toggle();
+    });
+    
+    // Hide profile dropdown if click is outside of dropdown
+    $(document).on('click', function(e) {
+        if (!$(e.target).closest('#header-profile').length) {
+            $('#header-profile-dropdown').hide();
+        }
+        
+        if(!$(e.target).closest('#header-messages').length) {
+            $('#header-messages-dropdown').hide();
+        }
+    });
+    
+    
 });

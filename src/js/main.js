ec1783d6bb7317ed59c496aae1a780328134c6e0
Arne Sierens <arne.srns@gmail.com>
2015-10-13 20:34:14 +0200

    Layout dropdown main search in progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
     
     // Search input on focus
-    $('#global-search-field').focusin(function() {
-        $('#dropdown-search').show();
+    $('#js-search-main').focusin(function() {
+        $('#js-search-main-dropdown').show();
     });
     
-    $('#global-search-field').focusout(function() {
-        $('#dropdown-search').hide();
+    $('#js-search-main').focusout(function() {
+        $('#js-search-main-dropdown').hide();
     });
     
     // Profile dropdown
-    $('#header-profile').click(function(e) {
-        e.preventDefault();
-        $('#header-profile-dropdown').toggle();
-    });
-    
-    // Messages dropdown
-    $('#header-messages').click(function(e) {
-        e.preventDefault();
-        $('#header-messages-dropdown').toggle();
-    });
-    
-    // Hide profile dropdown if click is outside of dropdown
-    $(document).on('click', function(e) {
-        if (!$(e.target).closest('#header-profile').length) {
-            $('#header-profile-dropdown').hide();
-        }
-        
-        if(!$(e.target).closest('#header-messages').length) {
-            $('#header-messages-dropdown').hide();
-        }
-    });
+//    $('#header-profile').click(function(e) {
+//        e.preventDefault();
+//        $('#header-profile-dropdown').toggle();
+//    });
+//    
+//    // Messages dropdown
+//    $('#header-messages').click(function(e) {
+//        e.preventDefault();
+//        $('#header-messages-dropdown').toggle();
+//    });
+//    
+//    // Hide profile dropdown if click is outside of dropdown
+//    $(document).on('click', function(e) {
+//        if (!$(e.target).closest('#header-profile').length) {
+//            $('#header-profile-dropdown').hide();
+//        }
+//        
+//        if(!$(e.target).closest('#header-messages').length) {
+//            $('#header-messages-dropdown').hide();
+//        }
+//    });
 });
